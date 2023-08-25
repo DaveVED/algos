bdaa85de5f80b6cca04672f6ec8f4cecb1b8e4ea
DaveVED <dave.w.dennis@gmail.com>
2023-08-25 15:23:41 -0500

    bubble sort

--- sort/bubble-sort.go
+++ sort/bubble-sort.go
@@ -4,6 +4,19 @@ import (
 	"fmt"
 )
 
+func bubbleSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr [j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
+
 func main() {
-	fmt.Println("HI")
+	arr := []int{1,2,7,4,2}
+	bubbleSort(arr)
+	fmt.Println(arr)
 }
